70e44e485c77a0840f67808c681196115e7e86ac
Alfred de Vulpian <derfladv@gmail.com>
2024-01-29 14:34:21 +0100

    Cleaner code, you can now search for a team matches

--- main.py
+++ main.py
@@ -2,9 +2,43 @@ import requests
 from datetime import date
 
 
+def search_match_infos(key, value, match_infos, team_name=None):
+    if key == "event":
+        match_infos["event_name"] = value["name"]
+        match_infos["event_region"] = value["region"]
+    if key == "blue":
+        match_infos["blue_team"] = value["team"]["team"]["name"]
+        try:
+            match_infos["blue_score"] = value["score"]
+        except KeyError:
+            pass
+    elif key == "orange":
+        match_infos["orange_team"] = value["team"]["team"]["name"]
+        try:
+            match_infos["orange_score"] = value["score"]
+        except KeyError:
+            pass
+    elif key == "date":
+        match_infos["date"] = value
+    elif key == "format":
+        match_infos["format"] = f"{value['type']} of {value['length']}"
+    elif key == "stage":
+        match_infos["stage_name"] = value["name"]
+
+    return match_infos
+
+
+def show_match_infos(match_infos):
+    print("\nNew match :")
+    for key, value in match_infos.items():
+        print(f"\t{key} : {value}")
+
+
 def get_upcoming_events(date):
     try:
-        response = requests.get(f"https://zsr.octane.gg/events?after={date}", verify=False)
+        response = requests.get(
+            f"https://zsr.octane.gg/events?after={date}", verify=False
+        )
         r_json = response.json()
         print(f"Response status : {response.status_code}\n")
         print(f"Upcoming events after {date} :")
@@ -24,81 +58,63 @@ def get_upcoming_events(date):
 
 def get_upcoming_matches(date):
     try:
-        response = requests.get(f"https://zsr.octane.gg/matches?after={date}", verify=False)
+        response = requests.get(
+            f"https://zsr.octane.gg/matches?after={date}", verify=False
+        )
         r_json = response.json()
         print(f"Response status : {response.status_code}\n")
         print(f"Upcoming matches after {date} :")
 
         for match in r_json["matches"]:
-            stage_name = "TBD"
-            orange = "TBD"
-            blue = "TBD"
-            orange_score = "0"
-            blue_score = "0"
-            event_name = "TBD"
-            event_region = "TBD"
-            date = "TBD"
-            format = "TBD"
-
-            print("New match :")
+            match_infos = {
+                "event_name": "TBD",
+                "event_region": "TBD",
+                "stage_name": "TBD",
+                "blue_team": "TBD",
+                "orange_team": "TBD",
+                "date": "TBD",
+                "format": "TBD",
+                "blue_score": "TBD",
+                "orange_score": "TBD",
+            }
             for key, value in match.items():
-                if key == "event":
-                    event_name = value["name"]
-                    event_region = value["region"]
-                if key == "blue":
-                    blue = value["team"]["team"]["name"]
-                    try:
-                        blue_score = value["score"]
-                    except KeyError:
-                        pass
-                elif key == "orange":
-                    orange = value["team"]["team"]["name"]
-                    try:
-                        orange_score = value["score"]
-                    except KeyError:
-                        pass
-                elif key == "date":
-                    date = value
-                elif key == "format":
-                    format = f"{value['type']} of {value['length']}"
-                elif key == "stage":
-                    stage_name = value["name"]
+                match_infos = search_match_infos(key, value, match_infos)
 
-            print(f"\tEvent : {event_name}")
-            print(f"\tRegion : {event_region}")
-            print(f"\tStage : {stage_name}")
-            print(f"\tBlue : {blue}")
-            print(f"\tOrange : {orange}")
-            print(f"\tDate : {date}")
-            print(f"\tFormat : {format}")
-            if blue_score and orange_score:
-                print(f"\tScore : {blue} : {blue_score} - {orange_score} : {orange}")
-            print()
+            show_match_infos(match_infos)
 
         return r_json
     except Exception as e:
         print(f"Error :\n{e}")
 
 
-def get_match_games(match_id):
+def get_team_all_matches(team_name, date):
     try:
-        response = requests.get(f"https://zsr.octane.gg/matches{match_id}/games", verify=False)
-        r_json = response.json()
-        print(f"Response status : {response.status_code}\n")
-        print(f"Games of match {match_id} :")
+        print(f"Matches of {team_name} after {date} :")
+        matches_json = requests.get(
+            f"https://zsr.octane.gg/matches?after={date}", verify=False
+        ).json()
 
-        for game in r_json["games"]:
-            print("New game :")
-            print(f"\tMatch : {game['match']['id']}")
-            print(f"\tGame : {game['number']}")
-            print(f"\tDate : {game['date']}")
-            print(f"\tBlue : {game['blue']['team']['team']['name']}")
-            print(f"\tOrange : {game['orange']['team']['team']['name']}")
-            print(f"\tBlue score : {game['blue']['team']['stats']['core']['goals']}")
-            print(f"\tOrange score : {game['orange']['team']['stats']['core']['goals']}")
-            print()
+        for match in matches_json["matches"]:
+            match_infos = {
+                "event_name": "TBD",
+                "event_region": "TBD",
+                "stage_name": "TBD",
+                "blue_team": "TBD",
+                "orange_team": "TBD",
+                "date": "TBD",
+                "format": "TBD",
+                "blue_score": "TBD",
+                "orange_score": "TBD",
+            }
 
-        return r_json
+            for key, value in match.items():
+                match_infos = search_match_infos(key, value, match_infos)
+
+            if (
+                match_infos["blue_team"] == team_name
+                or match_infos["orange_team"] == team_name
+            ):
+                show_match_infos(match_infos)
     except Exception as e:
         print(f"Error :\n{e}")
 
@@ -107,8 +123,9 @@ def main():
     today = date.today()
     print(f"Today's date : {today}\n")
 
-    get_upcoming_events(today)
-    get_upcoming_matches(today)
+    # get_upcoming_events(today)
+    # get_upcoming_matches(today)
+    get_team_all_matches("Gros Noobz", today)
 
 
 if __name__ == "__main__":
