6b5e21897a755147995f8fa63c7781ca7f0bbe1d
Alfred de Vulpian <derfladv@gmail.com>
2024-01-31 14:17:27 +0100

    new feature : Added notification buttons to matches.html, so that users can get notifications for choosen upcoming matches

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-from main import get_matches, get_events
+from flask import Flask, render_template, request, redirect, url_for
+from main import get_matches, get_events, show_infos, send_notification
+import json
 
 app = Flask(__name__)
 
@@ -9,11 +10,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/test")
-def test():
-    return render_template("test.html")
-
-
 @app.route("/matches", methods=["GET", "POST"])
 def matches():
     if request.method == "POST":
@@ -21,12 +17,27 @@ def matches():
         region = request.form.get("region")
         event = request.form.get("event")
         after = request.form.get("after")
+
         print(f"team : {team}\nregion : {region}\nevent : {event}\nafter : {after}")
+
         return render_template(
             "matches.html", matches=get_matches(team, region, event, after)
         )
     else:
         return render_template("matches.html", matches=get_matches())
+
+
+@app.route("/notify", methods=["POST"])
+def notify():
+    if request.method == "POST":
+        match_info = request.form.get("match_info").replace("'", '"')
+        match_info_json = json.loads(match_info)
+
+        print("You will be notified before this match starts :")
+        show_infos(type="Match", infos=match_info_json)
+        send_notification(match_info_json)
+
+        return '', 204
 
 
 @app.route("/events", methods=["GET", "POST"])
@@ -35,6 +46,7 @@ def events():
         region = request.form.get("region")
         event_name = request.form.get("event_name")
         after = request.form.get("after")
+
         return render_template(
             "events.html", events=get_events(region, event_name, after)
         )
